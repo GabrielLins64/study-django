12d4fe51fe90c6652053c9f32e1afacf6fcf8713
Gabriel Lins <gabrielinsmelo@gmail.com>
2022-11-26 16:11:42 -0300

    feat: custom polls admin

--- server/polls/models.py
+++ server/polls/models.py
@@ -1,5 +1,6 @@
 import datetime
 
+from django.contrib import admin
 from django.db import models
 from django.utils import timezone
 
@@ -7,6 +8,12 @@ from django.utils import timezone
 class Question(models.Model):
     def __str__(self):
         return self.question_text
+
+    # @admin.display(
+    #     boolean=True,
+    #     ordering='pub_date',
+    #     description='Published recently?',
+    # )
 
     def was_published_recently(self):
         now = timezone.now()
