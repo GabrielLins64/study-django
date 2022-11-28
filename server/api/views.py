047b99ff19fa75e0fcc007e6bd43e4fc0241a9f9
Gabriel Lins <gabrielinsmelo@gmail.com>
2022-11-28 16:35:46 -0300

    add endpoint for snippet

--- server/api/views.py
+++ server/api/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets, permissions
-from api.serializers import UserSerializer, GroupSerializer
+from api.models import Snippet
+from api.serializers import UserSerializer, GroupSerializer, SnippetSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -18,4 +19,13 @@ class GroupViewSet(viewsets.ModelViewSet):
     """
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class SnippetViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
     permission_classes = [permissions.IsAuthenticated]
