41dc482109b881cbb1c433df822f2086c760a278
GabrielLins64 <gabrielinsmelo@gmail.com>
2022-11-30 11:52:26 -0300

    add root api view

--- server/api/views.py
+++ server/api/views.py
@@ -1,12 +1,32 @@
 from django.contrib.auth.models import User, Group
 from django.http import HttpResponse, JsonResponse, Http404
-from rest_framework import viewsets, permissions, status, generics
+from rest_framework import viewsets, permissions, status, generics, renderers
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from api.models import Snippet
 from api.serializers import UserSerializer, GroupSerializer, SnippetSerializer
 from api.permissions import IsOwnerOrReadOnly
+from rest_framework.reverse import reverse
+
+
+@api_view(['GET'])
+@permission_classes((permissions.AllowAny,))
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
 
 
 class UserList(generics.ListAPIView):
